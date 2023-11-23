f9d40be26da070a6bf862e8d5e6d60b389cc8de1
Bharat Yadav <bharat.yadav@zehntech.com>
2023-11-23 18:06:38 +0530

    reading html data using code

---
+++ src/webparts/helloWorld/HelloWorldWebPart.ts
@@ -0,0 +1,132 @@
+import { Version } from '@microsoft/sp-core-library';
+import {
+  type IPropertyPaneConfiguration,
+  PropertyPaneTextField
+} from '@microsoft/sp-property-pane';
+import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
+//import type { IReadonlyTheme } from '@microsoft/sp-component-base';
+//import { escape } from '@microsoft/sp-lodash-subset';
+
+import styles from './HelloWorldWebPart.module.scss';
+import * as strings from 'HelloWorldWebPartStrings';
+
+export interface IHelloWorldWebPartProps {
+  description: string;
+}
+
+export default class HelloWorldWebPart extends BaseClientSideWebPart<IHelloWorldWebPartProps> {
+
+  // private _isDarkTheme: boolean = false;
+  // private _environmentMessage: string = '';
+
+  public render(): void {
+    this.domElement.innerHTML = `
+    <section class="${styles.helloWorld} ${!!this.context.sdks.microsoftTeams ? styles.teams : ''}">
+        
+    <label>Enter List name</label>
+    <input id = "input1" type="text" name = "listname"><br>
+    <label>Enter description</label>
+    <input id = "input2" type = "text" name = "description">
+    <button id = "submit">create List</button>
+
+
+    </section>`;
+    this.domElement.querySelector("#submit")?.addEventListener("click",()=>{
+           this.submit();
+    })
+  }
+
+  private submit():void{
+     const input1 = this.domElement.querySelector("#input1") as HTMLInputElement;
+
+     const input2 = document.getElementById("input2") as HTMLInputElement;
+     
+
+     const  listname = input1.value
+     console.log(listname);
+     console.log(input2.value);
+     
+    
+  }
+
+
+
+  // protected onInit(): Promise<void> {
+  //   return this._getEnvironmentMessage().then(message => {
+  //     this._environmentMessage = message;
+  //   });
+  // }
+
+
+
+  // private _getEnvironmentMessage(): Promise<string> {
+  //   if (!!this.context.sdks.microsoftTeams) { // running in Teams, office.com or Outlook
+  //     return this.context.sdks.microsoftTeams.teamsJs.app.getContext()
+  //       .then(context => {
+  //         let environmentMessage: string = '';
+  //         switch (context.app.host.name) {
+  //           case 'Office': // running in Office
+  //             environmentMessage = this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentOffice : strings.AppOfficeEnvironment;
+  //             break;
+  //           case 'Outlook': // running in Outlook
+  //             environmentMessage = this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentOutlook : strings.AppOutlookEnvironment;
+  //             break;
+  //           case 'Teams': // running in Teams
+  //           case 'TeamsModern':
+  //             environmentMessage = this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentTeams : strings.AppTeamsTabEnvironment;
+  //             break;
+  //           default:
+  //             environmentMessage = strings.UnknownEnvironment;
+  //         }
+
+  //         return environmentMessage;
+  //       });
+  //   }
+
+  //   return Promise.resolve(this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentSharePoint : strings.AppSharePointEnvironment);
+  // }
+
+  // protected onThemeChanged(currentTheme: IReadonlyTheme | undefined): void {
+  //   if (!currentTheme) {
+  //     return;
+  //   }
+
+  //   this._isDarkTheme = !!currentTheme.isInverted;
+  //   const {
+  //     semanticColors
+  //   } = currentTheme;
+
+  //   if (semanticColors) {
+  //     this.domElement.style.setProperty('--bodyText', semanticColors.bodyText || null);
+  //     this.domElement.style.setProperty('--link', semanticColors.link || null);
+  //     this.domElement.style.setProperty('--linkHovered', semanticColors.linkHovered || null);
+  //   }
+
+  // }
+
+  protected get dataVersion(): Version {
+    return Version.parse('1.0');
+  }
+
+  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+    return {
+      pages: [
+        {
+          header: {
+            description: strings.PropertyPaneDescription
+          },
+          groups: [
+            {
+              groupName: strings.BasicGroupName,
+              groupFields: [
+                PropertyPaneTextField('description', {
+                  label: strings.DescriptionFieldLabel
+                })
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
+}
